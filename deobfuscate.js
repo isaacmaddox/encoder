75b76f1c3c8ede996167ec70dcb66cba6580b4ac
isaacmaddox <92178904+isaacmaddox@users.noreply.github.com>
2022-02-24 13:31:20 -0600

    Add files via upload

--- deobfuscate.js
+++ deobfuscate.js
@@ -1,48 +1,23 @@
-// "Isaac" = {45d*wP5Y5Dz2"4f[.sowc~X9neCZ]/5B)_7N@mK`&+}MQG|0'SuJ\1tx?!UIbrO#p -R6q$FP>gDV;T8Edhi3av^l<%,kWALH=j(y:
-// Key = 45dwP55D
-// Extract ASCII character string (starting at index 0, then at every parseInt(string[1]) index)
-// Get key (611u)
+var array = [];
 
 function decode(str) {
-    var asciiIndex = parseInt(str[1]);
-    var chars = str[0];
-    var stringLength = ""; // Length of inputted string, not encoded string
-    var string = str.split("");
-    var keyLength = 0;
-
-    if (string[3] !== "d" && string[3] !== "u") {
-        stringLength += string[2] + string[3];
-    } else {
-        stringLength = string[2];
-    }
-    stringLength = parseInt(stringLength);
-
-    var numOfTimesAsciiIsInKey = Math.floor(stringLength / asciiIndex);
-    console.error(numOfTimesAsciiIsInKey);
-
-    // save last index, if char is undefined, go back to index + 1
-    var lastIndex = 0;
-    for (var i = 1; i < 94; i++) {
-        var char = "";
-        lastIndex = i;
-        if (i == 1) {
-            char = string[asciiIndex];
-            string.splice(asciiIndex, 1);
+    var de = str.split("");
+    var shift = parseInt(str[0]);
+    var dir = str[1];
+    de.splice(0, 2);
+    var ascii = de.splice(de.length - 95, de.length);
+    let final = "";
+    for (var i = 0; i < de.length; i++) {
+        var ind = ascii.indexOf(de[i]);
+        var newInd = 0;
+        if (dir == "u") newInd = ind - shift
+        else newInd = ind + shift;
+        if (newInd > ascii.length - 1) {
+            newInd = newInd - ascii.length;
+        } else if (newInd < 0) {
+            newInd = ascii.length + newInd;
         }
-        else if (i > string.length) {
-            char = string[lastIndex + 1];
-        } else {
-            char = string[i * asciiIndex];
-            string.splice(i * asciiIndex, 1);
-        }
-        console.log(char); // REMOVIENOVWE
-
-        chars += char;
+        final += ascii[newInd];
     }
-    string.splice(0, 1);
-    console.log(string);
-    console.log(stringLength);
-
+    console.log(final);
 }
-
-// #TODO: fix inc being rounded in key (dumbass).
--- obfuscate.js
+++ obfuscate.js
@@ -1,100 +1,65 @@
-// Encoder
-/*
-  Random shuffle an array of characters
-  Take a string from user input
-  Generate random number, num of char to shuffle by, and a letter "u" or "d" (up or down)
-  Shuffle each character in the string
-  Put encoder key (i.e. "3u") in the first two chars
-  Hide the shuffled characters string in every nth index (the encoder key num) in the string; append remaining characters at the end of the string OR use dummy characters to keep number at every index
-    - if n > 10, set n = n - 10
-
-  Hide key in the xth index, where x = the constant in the encoder key
-  Keep length of original string in the beginning so as to aid in decoding (optional)
-*/
-
 var ascii =
     " !\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~";
-
 function encode(str) {
-    let chars = ascii.split("");
-    const shuffled = shuffle(chars);
-    chars = "";
-    for (var i = 0; i < shuffled.length; i++) {
-        chars += shuffled[i];
-    }
-
-    var inc = Math.floor(Math.random() * ascii.length);
-    inc = inc == 3 ? 2 : inc; // invalidate 3 as an inc value to prevent 
-    var ud = Math.floor(Math.random() * 2);
-    ud = ud == 1 ? "u" : "d";
-
-    // Go through each character of str and shift them either up or down by inc
-    // str = str.split("");
-    let encoded = "";
-    for (var j = 0; j < str.length; j++) {
-        var storeInc = 0;
-        var incChanged = false;
-        var index = chars.indexOf(str[j]);
-        if (index + inc > chars.length) {
-            index = (index + inc) - chars.length;
-            storeInc = inc;
-            incChanged = true;
-            inc = 0;
-        } else if (index - inc < 0) {
-            index = chars.length + (index - inc);
-            storeInc = inc;
-            incChanged = true;
-            inc = 0;
+    var chars = ascii.split("");
+    chars = shuffle(chars);
+    let shift = Math.floor(Math.random() * 9);
+    shift = shift == 0 ? 2 : shift;
+    let ud = Math.floor(Math.random() * 2) == 1 ? "u" : "d";
+    let final = shift.toString() + ud;
+    for (var i = 0; i < str.length; i++) {
+        var ind = chars.indexOf(str[i]);
+        if (ud == "u") newInd = ind + shift
+        else if (ud == "d") newInd = ind - shift;
+        if (newInd > chars.length) {
+            newInd = newInd - chars.length;
+        } else if (newInd < 0) {
+            newInd = chars.length + newInd;
         }
-        // console.log(index + inc);
-        if (ud == "u") encoded += chars[index + inc];
-        else encoded += chars[index - inc];
-
-        if (incChanged) inc = storeInc
-        else inc = inc;
+        final += chars[newInd];
     }
-
-    // Adding key to string
-    // If inc > 10 set inc to Math.floor(inc / 10)
-    inc = inc == 3 ? 2 : inc; // invalidate 3 as an inc value to prevent 
-    if (inc > 10) inc = Math.floor(inc / 10);
-
-    var output = "";
-    var final = "";
-
-    // Add "key" to beginning
-    output += inc.toString() + str.length.toString() + ud + encoded;
-    console.warn("Key: " + output);
-    output = output.split("");
-
-    for (var a = 0; a < chars.length; a++) {
-        output.splice(a * inc, 0, chars[a]);
+    for (var j = 0; j < chars.length; j++) {
+        final += chars[j];
     }
-
-    for (var b = 0; b < output.length; b++) {
-        final += output[b];
-    }
-
     console.log(final);
-    return encoded;
 }
-
 function shuffle(array) {
-    let currentIndex = array.length,
-        randomIndex;
-
-    // While there remain elements to shuffle...
+    let currentIndex = array.length, randomIndex;
     while (currentIndex != 0) {
-        // Pick a remaining element...
         randomIndex = Math.floor(Math.random() * currentIndex);
         currentIndex--;
-
-        // And swap it with the current element.
-        [array[currentIndex], array[randomIndex]] = [
-            array[randomIndex],
-            array[currentIndex]
-        ];
+        [array[currentIndex], array[randomIndex]] = [array[randomIndex], array[currentIndex]];
     }
-
     return array;
 }
+
+var array = ["Isaac", "Steph"];
+var result = [];
+
+function arrencode(str) {
+    for (var e = 0; e < str.length; e++) {
+        var chars = ascii.split("");
+        chars = shuffle(chars);
+        let shift = Math.floor(Math.random() * 9);
+        shift = shift == 0 ? 2 : shift;
+        let ud = Math.floor(Math.random() * 2) == 1 ? "u" : "d";
+        let final = shift.toString() + ud;
+        for (var i = 0; i < str[e].length; i++) {
+            var ind = chars.indexOf(str[e][i]);
+            if (ud == "u") newInd = ind + shift
+            else if (ud == "d") newInd = ind - shift;
+            if (newInd > chars.length) {
+                newInd = newInd - chars.length;
+            } else if (newInd < 0) {
+                newInd = chars.length + newInd;
+            }
+            final += chars[newInd];
+        }
+        for (var j = 0; j < chars.length; j++) {
+            final += chars[j];
+        }
+        console.error(final);
+        navigator.clipboard.writeText(final);
+    }
+}
+arrencode(array);
